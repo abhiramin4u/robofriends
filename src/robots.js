73c97bdfb6798a13aa191cc6a346da1b14a2692e
AR <abhiramin4u@gmail.com>
2018-09-19 16:00:07 +0530

    Adding Files

---
+++ src/robots.js
@@ -0,0 +1,44 @@
+export const robots = [
+  {
+    id:1,
+    name: 'Monna',
+    username: 'Monna1',
+    email:'monna1@gmail.com'
+  },
+  {
+    id:2,
+    name: 'Umminika',
+    username: 'Monna2',
+    email:'monna2@gmail.com'
+  },
+  {
+    id:3,
+    name: 'Paaru',
+    username: 'Monna3',
+    email:'monna3@gmail.com'
+  },
+  {
+    id:4,
+    name: 'Kunjipaaru',
+    username: 'Monna4',
+    email:'monna4@gmail.com'
+  },
+  {
+    id:5,
+    name: 'Kumba',
+    username: 'Monna5',
+    email:'monna5@gmail.com'
+  },
+  {
+    id:6,
+    name: 'Penn',
+    username: 'Monna6',
+    email:'monna6@gmail.com'
+  },
+  {
+    id:7,
+    name: 'Paathuma',
+    username: 'Monna7',
+    email:'monna6@gmail.com'
+  },
+]
